2a22470b9376d840bf51abb91dce4891c0c9f38c
Vasiliy Zukanov <vasiliy.zukanov@gmail.com>
2015-02-08 19:19:50 +0200

    Initial implementation of Navigation Drawer.

--- app/src/main/java/il/co/idocare/www/idocare/IDoCareActivity.java
+++ app/src/main/java/il/co/idocare/www/idocare/IDoCareActivity.java
@@ -1,11 +1,20 @@
 package il.co.idocare.www.idocare;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.text.Layout;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -41,6 +50,8 @@ public class IDoCareActivity extends Activity implements
         initUniversalImageLoader();
 
         buildGoogleApiClient();
+
+        initNavigationDrawer();
 
         // This callback will be used to show/hide up (back) button in actionbar
         getFragmentManager().addOnBackStackChangedListener(this);
@@ -110,6 +121,87 @@ public class IDoCareActivity extends Activity implements
     }
 
     /**
+     * Show a particular fragment
+     * TODO: maybe we need to preserve the state of the replaced fragments?
+     * @param claz the class of the fragment to be shown
+     */
+    protected void showFragment(Class<? extends Fragment> claz, boolean addToBackStack) {
+
+        Fragment currFragment = getFragmentManager().findFragmentById(R.id.frame_contents);
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+
+        if (currFragment != null) {
+            if (claz.isInstance(currFragment)) {
+                // The currently shown fragment is the same as the new one - nothing to do
+                Log.v(LOG_TAG, "the fragment " + claz.getSimpleName() + " is already shown");
+                return;
+            }
+        }
+
+        // Create new fragment
+        Fragment newFragment = null;
+
+        try {
+            newFragment = claz.newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        if (addToBackStack) ft.addToBackStack(null);
+        // Change to a new fragment
+        ft.replace(R.id.frame_contents, newFragment, claz.getClass().getSimpleName());
+        ft.commit();
+
+    }
+
+    /**
+     * Initiate the navigation drawer
+     */
+    private void initNavigationDrawer() {
+        String[] entries = getResources().getStringArray(R.array.drawer_entries);
+        final DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        final ListView drawerList = (ListView) findViewById(R.id.drawer_contents);
+
+        // Set the adapter for the list view
+        drawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1
+                , entries));
+
+
+        drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+                // Highlight the selected item and close the drawer
+                drawerList.setItemChecked(position, true);
+                drawerLayout.closeDrawer(drawerList);
+
+                switch (position) {
+
+                    // TODO: populate other options too
+                    case 0:
+                        showFragment(FragmentHome.class, true);
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                }
+
+                // Clear back-stack
+                // TODO: this is correct only if all entries in the drawer are "top level"
+                getFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+
+            }
+        });
+
+    }
+
+
+    /**
      * Handle the initiation of UIL (third party package under Apache 2.0 license)
      */
     private void initUniversalImageLoader() {
@@ -128,6 +220,7 @@ public class IDoCareActivity extends Activity implements
                 .build();
     }
 
+
     @Override
     public void onConnected(Bundle bundle) {
 
@@ -142,7 +235,6 @@ public class IDoCareActivity extends Activity implements
     public void onConnectionFailed(ConnectionResult connectionResult) {
 
     }
-
 
 
     /**
@@ -172,9 +264,10 @@ public class IDoCareActivity extends Activity implements
                 mRequestsUpdateScheduler.scheduleAtFixedRate (update, 30, 30, TimeUnit.SECONDS);
     }
 
+    /**
+     * Stop updates of requests' cache
+     */
     private void stopRequestsCacheUpdates() {
-//        if (mRequestsUpdateScheduler != null)
-//            mRequestsUpdateScheduler.shutdown();
         if (mScheduledFuture != null) mScheduledFuture.cancel(false);
     }
 
@@ -188,4 +281,9 @@ public class IDoCareActivity extends Activity implements
             Log.e(LOG_TAG, "serverResponse was called with unrecognized tag: " + tag.toString());
         }
     }
+
+
+
+
+
 }
