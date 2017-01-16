3bc5f893ec27439556bd07a16c63b9f861df815a
Vasiliy Zukanov <vasiliy.zukanov@gmail.com>
2017-01-16 09:41:34 +0200

    Redesign of PromptDialog

--- app/src/main/java/il/co/idocare/dialogs/PromptDialog.java
+++ app/src/main/java/il/co/idocare/dialogs/PromptDialog.java
@@ -6,14 +6,20 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import org.greenrobot.eventbus.EventBus;
 
 import javax.inject.Inject;
 
+import il.co.idocare.R;
 import il.co.idocare.dialogs.events.PromptDialogDismissedEvent;
-import il.co.idocare.utils.Logger;
 
 /**
  * A dialog that can show title and message and has two buttons. User's actions performed
@@ -29,39 +35,62 @@ public class PromptDialog extends BaseDialog {
 
     @Inject EventBus mEventBus;
 
+    private TextView mTxtTitle;
+    private TextView mTxtMessage;
+    private Button mBtnPositive;
+    private Button mBtnNegative;
+
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         getControllerComponent().inject(this);
     }
 
+    @Nullable
     @Override
-    public @NonNull Dialog onCreateDialog(Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.dialog_info_prompt, container, false);
+
+        initSubViews(rootView);
+
+        populateSubViews();
+
+        return rootView;
+
+    }
+
+    private void initSubViews(View rootView) {
+        mTxtTitle = (TextView) rootView.findViewById(R.id.txt_dialog_title);
+        mTxtMessage = (TextView) rootView.findViewById(R.id.txt_dialog_message);
+        mBtnPositive = (Button) rootView.findViewById(R.id.btn_dialog_positive);
+        mBtnNegative = (Button) rootView.findViewById(R.id.btn_dialog_negative);
+
+        mBtnPositive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEventBus.post(new PromptDialogDismissedEvent(getDialogTag(), PromptDialogDismissedEvent.BUTTON_POSITIVE));
+            }
+        });
+
+        mBtnNegative.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEventBus.post(new PromptDialogDismissedEvent(getDialogTag(), PromptDialogDismissedEvent.BUTTON_NEGATIVE));
+            }
+        });
+    }
+
+    private void populateSubViews() {
         String title = getArguments().getString(ARG_TITLE);
         String message = getArguments().getString(ARG_MESSAGE);
         String positiveButtonCaption = getArguments().getString(ARG_POSITIVE_BUTTON_CAPTION);
         String negativeButtonCaption = getArguments().getString(ARG_NEGATIVE_BUTTON_CAPTION);
 
-        Dialog dialog = new AlertDialog.Builder(getActivity())
-                .setTitle(TextUtils.isEmpty(title) ? "" : title)
-                .setMessage(TextUtils.isEmpty(message) ? "" : message)
-                .setPositiveButton(TextUtils.isEmpty(positiveButtonCaption) ? "" : positiveButtonCaption,
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                mEventBus.post(new PromptDialogDismissedEvent(getDialogTag(), PromptDialogDismissedEvent.BUTTON_POSITIVE));
-                            }
-                        })
-                .setNegativeButton(TextUtils.isEmpty(negativeButtonCaption) ? "" : negativeButtonCaption,
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                mEventBus.post(new PromptDialogDismissedEvent(getDialogTag(), PromptDialogDismissedEvent.BUTTON_NEGATIVE));
-                            }
-                        })
-                .create();
-
-        return dialog;
+        mTxtTitle.setText(TextUtils.isEmpty(title) ? "" : title);
+        mTxtMessage.setText(TextUtils.isEmpty(message) ? "" : message);
+        mBtnPositive.setText(positiveButtonCaption);
+        mBtnNegative.setText(negativeButtonCaption);
     }
 
 }
