b094f7106860672482dbef732225462a5271a577
Vasiliy Zukanov <vasiliy.zukanov@gmail.com>
2017-08-06 14:49:10 +0300

    Removing dependency on GET_ACCOUNTS permission and bumping version

--- app/src/main/java/il/co/idocare/authentication/StubAccountAuthenticator.java
+++ app/src/main/java/il/co/idocare/authentication/StubAccountAuthenticator.java
@@ -3,15 +3,9 @@ package il.co.idocare.authentication;
 import android.accounts.AbstractAccountAuthenticator;
 import android.accounts.Account;
 import android.accounts.AccountAuthenticatorResponse;
-import android.accounts.AccountManager;
 import android.accounts.NetworkErrorException;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
-
-import il.co.idocare.controllers.activities.LoginActivity;
 /**
  * Implement AbstractAccountAuthenticator and stub out all
  * of its methods
