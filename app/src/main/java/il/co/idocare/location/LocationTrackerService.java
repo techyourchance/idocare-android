6c57b7c2497f9058c4b47196bc748b2b2918f906
Vasiliy Zukanov <vasiliy.zukanov@gmail.com>
2018-08-08 20:35:16 +0300

    Updating target version, gradle, dependencies and the whole world

--- app/src/main/java/il/co/idocare/location/LocationTrackerService.java
+++ app/src/main/java/il/co/idocare/location/LocationTrackerService.java
@@ -16,7 +16,7 @@ import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
--- app/src/main/java/il/co/idocare/pictures/CameraAdapter.java
+++ app/src/main/java/il/co/idocare/pictures/CameraAdapter.java
@@ -8,7 +8,6 @@ import android.os.Build;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
-import android.support.v4.BuildConfig;
 import android.support.v4.content.FileProvider;
 
 import java.io.File;
